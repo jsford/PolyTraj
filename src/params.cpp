e46d7144fc41ac308d89111b429b6bd63d43a724
Jordan <jsford@andrew.cmu.edu>
2018-04-09 13:40:31 -0400

    Moved Paths into PolyTraj::Path and Trajectories into PolyTraj::Trajectory.
    Signed-off-by: Jordan <jsford@andrew.cmu.edu>

--- src/params.cpp
+++ src/params.cpp
@@ -1,38 +1,45 @@
 #include "params.h"
 
 namespace PolyTraj {
+namespace Trajectory {
 
-TrajParams::TrajParams(double S, const Polynomial &kDotPoly,
-                       const Polynomial &vDotPoly)
+Params::Params(double S, const Polynomial &kDotPoly,
+               const Polynomial &vDotPoly)
   : S(S), kDotPoly(kDotPoly), vDotPoly(vDotPoly) {}
 
-TrajParams::TrajParams(const Eigen::VectorXd &params, int kDotDegree,
-                       int vDotDegree)
+Params::Params(const Eigen::VectorXd &params, int kDotDegree,
+               int vDotDegree)
   : S(params[0]),
     kDotPoly(params.segment(1, kDotDegree)),
     vDotPoly(params.tail(vDotDegree)) {
   assert(kDotDegree + vDotDegree + 1 == params.size());
 }
 
-double TrajParams::operator()(double s) const { return kDotPoly(s); }
+double Params::operator()(double s) const { return kDotPoly(s); }
 
-Eigen::VectorXd TrajParams::vector() const {
+Eigen::VectorXd Params::vector() const {
   Eigen::VectorXd vec(1 + kDotPoly.deg());
   vec << S, kDotPoly.coeffs;
   return vec;
 }
 
-PathParams::PathParams(double S, const Polynomial &kDotPoly)
+} // namespace Trajectory
+
+namespace Path {
+
+Params::Params(double S, const Polynomial &kDotPoly)
   : S(S), kDotPoly(kDotPoly) {}
 
-PathParams::PathParams(const Eigen::VectorXd &params)
+Params::Params(const Eigen::VectorXd &params)
   : S(params[0]), kDotPoly(params.tail(params.size() - 1)) {}
 
-double PathParams::operator()(double s) const { return kDotPoly(s); }
+double Params::operator()(double s) const { return kDotPoly(s); }
 
-Eigen::VectorXd PathParams::vector() const {
+Eigen::VectorXd Params::vector() const {
   Eigen::VectorXd vec(1 + kDotPoly.deg());
   vec << S, kDotPoly.coeffs;
   return vec;
 }
-}  // namespace PolyTraj
+
+} // namespace Path
+} // namespace PolyTraj
