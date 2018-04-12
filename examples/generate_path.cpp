7d1161e65e26702e175dfad8b4fba260975a4394
Jordan <jsford@andrew.cmu.edu>
2018-04-12 00:29:34 -0400

    Separated the library compilation into its own CMakeLists.txt file. Also formatted the examples to be more informative.

--- examples/generate_path.cpp
+++ examples/generate_path.cpp
@@ -11,23 +11,31 @@ int main() {
   State xs, xe;
 
   // The path start state.
-  xs << 0,          // x = 0
-        0,          // y = 0
-        M_PI_2,     // theta = pi/2
-        0.0;        // curvature = 0
+  xs << 0,     // x = 0
+      0,       // y = 0
+      M_PI_2,  // theta = pi/2
+      0.0;     // curvature = 0
 
   // The path end state.
-  xe << 10,         // x = 10
-        10,         // y = 10
-        M_PI_2,     // theta = pi/2
-        0.0;        // curvature = 0
+  xe << 10,      // x = 10
+        10,      // y = 10
+        M_PI_2,  // theta = pi/2
+        0.0;     // curvature = 0
 
   // Generate 100 points along a path from xs to xe.
   // Use a 4th order polynomial to parameterize the change in steering. (dk/dt)
   Path path = generate(xs, xe, 4, 100);
 
+  Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+
+  std::cout << "Start Posture: " << xs.transpose().format(CleanFmt)
+            << std::endl;
+  std::cout << "End Posture:   " << xs.transpose().format(CleanFmt)
+            << std::endl;
+  std::cout << std::endl;
+
   // Pretty print the path. Each row is a state.
   // The first row is xs. The final row is (nearly) xe.
-  Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+  std::cout << "Generated Path: [x, y, theta, curvature]" << std::endl;
   std::cout << path.transpose().format(CleanFmt) << std::endl;
 }
--- examples/generate_trajectory.cpp
+++ examples/generate_trajectory.cpp
@@ -11,28 +11,39 @@ int main() {
   State xs, xe;
 
   // The trajectory start state.
-  xs << 0,          // x = 0
-        0,          // y = 0
-        M_PI_2,     // theta = pi/2
-        0.0,        // curvature = 0
-        1.0,        // velocity = 1
-        0.0;        // acceleration = 0
+  xs << 0,     // x = 0
+      0,       // y = 0
+      M_PI_2,  // theta = pi/2
+      0.0,     // curvature = 0
+      1.0,     // velocity = 1
+      0.0;     // acceleration = 0
 
   // The trajectory end state.
-  xe << 10,         // x = 10
-        10,         // y = 10
-        M_PI_2,     // theta = pi/2
-        0.0,        // curvature = 0
-        5.0,        // velocity = 1
-        0.0;        // acceleration = 0
+  xe << 10,    // x = 10
+      10,      // y = 10
+      M_PI_2,  // theta = pi/2
+      0.0,     // curvature = 0
+      5.0,     // velocity = 1
+      0.0;     // acceleration = 0
 
   // Generate 100 points along a trajectory from xs to xe.
   // Use a 4th order polynomial to parameterize the change in steering (dk/dt)
-  // and a 4th order polynomial to parameterize the change in acceleration (da/dt).
+  // and a 4th order polynomial to parameterize the change in acceleration
+  // (da/dt).
   Trajectory traj = generate(xs, xe, 4, 4, 100);
+
+  Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+
+  std::cout << "Start Posture: " << xs.transpose().format(CleanFmt)
+            << std::endl;
+  std::cout << "End  Posture:  " << xs.transpose().format(CleanFmt)
+            << std::endl;
+  std::cout << std::endl;
 
   // Pretty print the trajectory. Each row is a state.
   // The first row is xs. The final row is (nearly) xe.
-  Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
+  std::cout << "Generated Trajectory: [x, y, theta, curvature, velocity, "
+               "acceleration]"
+            << std::endl;
   std::cout << traj.transpose().format(CleanFmt) << std::endl;
 }
